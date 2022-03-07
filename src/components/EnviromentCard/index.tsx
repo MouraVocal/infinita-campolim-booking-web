4dc568aa72cde78d686e6c07a31e061194728079
MouraVocal <diego.moura3@gmail.com>
2022-03-07 16:26:23 -0300

    add environment cards

---
+++ src/components/EnviromentCard/index.tsx
@@ -0,0 +1,15 @@
+interface EnvironmentCardProps {
+  src: string;
+  title: string;
+}
+
+export function EnviromentCard ({ src, title }: EnvironmentCardProps) {
+  return (
+    <div className='align-items-center'>
+    <h6 className="text-center">{title}</h6>
+    <div className='container'>
+      <img className="rounded" src={src} alt="foto academia" />
+    </div>
+    </div>
+  )
+}
