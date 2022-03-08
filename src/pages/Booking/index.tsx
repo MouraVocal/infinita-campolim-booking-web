e57e37cf0b24245a29caffc0b7c0aa006e57dd28
MouraVocal <diego.moura3@gmail.com>
2022-03-08 11:31:45 -0300

    add datepicker

--- src/pages/Booking/index.tsx
+++ src/pages/Booking/index.tsx
@@ -15,22 +15,43 @@ import './styles.css'
 
 export const Booking: React.FC = () => {
   const navigate = useNavigate()
+
+  const environmentArrays = [
+    {
+      src: Academia,
+      title: 'Academia',
+      path: 'gym'
+    },
+    {
+      src: Piscina,
+      title: 'Piscina',
+      path: 'pool'
+    },
+    {
+      src: Salao,
+      title: 'Salão de Festas',
+      path: 'partyroom'
+    },
+    {
+      src: Quadra,
+      title: 'Quadra',
+      path: 'sportscourt'
+    }
+
+  ]
   return (
     <div className='container-fluid mb-3'>
       <h3 className='text-center my-4'>Por favor, escolha o ambiente</h3>
       <div className='d-flex flex-wrap align-items-center justify-content-center'>
-        <div className='onclick' onClick={() => navigate('/environments/gym')}>
-          <EnviromentCard src={Academia} title='Academia' />
-        </div>
-        <div className='onclick' onClick={() => navigate('/environments/pool')}>
-          <EnviromentCard src={Piscina} title='Piscina' />
-        </div>
-        <div className='onclick' onClick={() => navigate('/environments/partyroom')}>
-          <EnviromentCard src={Salao} title='Salão de Festas' />
-        </div>
-        <div className='onclick' onClick={() => navigate('/environments/sportscourt')}>
-          <EnviromentCard src={Quadra} title='Quadra' />
-        </div>
+        {
+          environmentArrays.map((item) => {
+            return (
+              <div key={item.title} className='onclick' onClick={() => navigate(`/environments/${item.path}`)}>
+                <EnviromentCard src={item.src} title={item.title} />
+              </div>
+            )
+          })
+        }
       </div>
     </div>
   )
--- src/pages/Environment/index.tsx
+++ src/pages/Environment/index.tsx
@@ -4,11 +4,33 @@ import { useParams } from 'react-router-dom'
 // Styles
 import './styles.css'
 
+// Excluding undefined in useParams types to use as an object value
+declare module 'react-router-dom' {
+  export function useParams<
+    P extends Record<string, string | undefined> = {
+      [key: string]: string | undefined;
+    }
+  >(): P;
+}
+
 export function Environment () {
-  const { environment } = useParams()
+  const { environment } = useParams<{environment: 'gym' | 'pool' | 'partyroom' | 'sportscourt'}>()
+
+  // Translating
+  const toPtBr = {
+    gym: 'Academia',
+    pool: 'Piscina',
+    partyroom: 'Salão de Festas',
+    sportscourt: 'Quadra'
+  }
+
   return (
     <>
-      <div id='head'>{environment}</div>
+      <div id='head'>{toPtBr[environment]}</div>
+      <div className='d-flex py-3 justify-content-center align-items-center'>
+        <label htmlFor="datepicker" className='px-3'>Escolha a data:</label>
+        <input type="date" name="datepicker" id="datepicker" className='form-control w-auto' />
+      </div>
     </>
   )
 }
