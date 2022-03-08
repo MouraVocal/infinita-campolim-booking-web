3d95996c4d58bb59204e941ee172b997fa16a8f5
MouraVocal <diego.moura3@gmail.com>
2022-03-08 13:09:37 -0300

    finish date picker

--- src/pages/Environment/index.tsx
+++ src/pages/Environment/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 // Routes
 import { useParams } from 'react-router-dom'
 
@@ -14,7 +16,11 @@ declare module 'react-router-dom' {
 }
 
 export function Environment () {
+  // Getting Params
   const { environment } = useParams<{environment: 'gym' | 'pool' | 'partyroom' | 'sportscourt'}>()
+
+  // States
+  const [date, setDate] = useState(new Date().toISOString().split('T')[0])
 
   // Translating
   const toPtBr = {
@@ -29,7 +35,14 @@ export function Environment () {
       <div id='head'>{toPtBr[environment]}</div>
       <div className='d-flex py-3 justify-content-center align-items-center'>
         <label htmlFor="datepicker" className='px-3'>Escolha a data:</label>
-        <input type="date" name="datepicker" id="datepicker" className='form-control w-auto' />
+        <input
+        type="date"
+        name="datepicker"
+        id="datepicker"
+        className='form-control w-auto'
+        value={date}
+        onChange={(value) => setDate(value.target.value)}
+        />
       </div>
     </>
   )
