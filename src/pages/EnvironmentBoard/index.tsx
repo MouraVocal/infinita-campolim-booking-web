21e2158debaf7dc1970a155c5ad4080156000848
MouraVocal <diego.moura3@gmail.com>
2022-03-17 16:06:38 -0300

    add posting and update openinghours

--- src/pages/EnvironmentBoard/index.tsx
+++ src/pages/EnvironmentBoard/index.tsx
@@ -4,14 +4,17 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 // Firebase
-import { collection, getDocs, DocumentData } from 'firebase/firestore'
+import { collection, getDocs, DocumentData, setDoc, doc, updateDoc, onSnapshot, query, orderBy } from 'firebase/firestore'
 import { db } from '../../config/firebase'
+import { getStorage, ref, getDownloadURL, uploadString } from 'firebase/storage'
 
 // Styles
 import './styles.css'
 
+// Assets
+import addPhotoIcon from '../../assets/addphoto.svg'
+
 // Components
-import { LoadingSpinner } from '../../components/LoadingSpinner'
 import AdminPostCard from '../../components/AdminPostCard'
 
 // Excluding undefined type in useParams types to use as an object entry
@@ -24,16 +27,20 @@ declare module 'react-router-dom' {
 }
 
 export function EnvironmentBoard () {
+  const storage = getStorage()
   const toPtBr = {
     gym: 'Academia',
     pool: 'Piscina',
     partyroom: 'Salão de Festas',
     sportscourt: 'Quadra'
   }
+
   const { environment } = useParams<{ environment: 'gym' | 'pool' | 'partyroom' | 'sportscourt' }>()
+
   const environmentEntry = toPtBr[environment].split(' ').join('')
+
   async function getEnviromentData () {
-    setLoading(true)
+    setLoadingOpeningHours(true)
     const docsRef = collection(db, 'boards')
     const querySnapshot = await getDocs(docsRef)
     querySnapshot.forEach(item => {
@@ -42,49 +49,232 @@ export function EnvironmentBoard () {
         setOpenedAt(item.data().openedAt)
       }
     })
-    setLoading(false)
+    setLoadingOpeningHours(false)
   }
 
   // States
-  const [loading, setLoading] = useState(false)
-  const [openedAt, setOpenedAt] = useState('')
-  const [closedAt, setClosedAt] = useState('')
+  const [loading] = useState(false)
+  const [loadingOpeningHours, setLoadingOpeningHours] = useState(false)
+  const [loadingPosting, setLoadingPosting] = useState(false)
+  const [openedAt, setOpenedAt] = useState(0)
+  const [closedAt, setClosedAt] = useState(0)
   const [posts, setPosts] = useState<DocumentData[]>([])
-
-  async function getBoards () {
-    setLoading(true)
-    setPosts([])
-    const docsRef = collection(db, `boards/${environmentEntry}/posts`)
-    const querySnapshot = await getDocs(docsRef)
-    querySnapshot.forEach(doc => {
-      setPosts(prevState => [...prevState, doc.data()])
-    })
-    setLoading(false)
-  }
+  const [isImgUploaded, setIsImgUploaded] = useState(false)
+  const [postImg, setPostImg] = useState(addPhotoIcon)
+  const [text, setText] = useState('')
 
   useEffect(() => {
-    setLoading(true)
-    getEnviromentData()
-    getBoards()
-    setLoading(false)
+    const docsRef = collection(db, `boards/${environmentEntry}/posts`)
+    const q = query(docsRef, orderBy('uid', 'desc'))
+    const unsubscribe = onSnapshot(q, querySnapShot => {
+      setLoadingPosting(true)
+      setPosts([])
+      querySnapShot.forEach(post => setPosts(prevState => [...prevState, post.data()]))
+      setLoadingPosting(false)
+    },
+    (error) => {
+      console.log(error)
+      setLoadingPosting(false)
+    }
+    )
+    return () => unsubscribe()
   }, [])
 
+  useEffect(() => {
+    getEnviromentData()
+  }, [])
+
+  const handleOpeningHours = async () => {
+    setLoadingOpeningHours(true)
+    const docRef = doc(db, 'boards', environmentEntry)
+    await updateDoc(docRef, {
+      openedAt: openedAt,
+      closedAt: closedAt
+    })
+      .then(() => {
+        console.log('Horário atualizado com sucesso!')
+        setLoadingOpeningHours(false)
+      })
+      .catch(error => {
+        console.log(error.message)
+        setLoadingOpeningHours(false)
+      })
+  }
+
+  const handlePost = async (uid: string) => {
+    setLoadingPosting(true)
+    const storageRef = ref(storage, `boards/${environmentEntry}/${uid}`)
+    await uploadString(storageRef, postImg as string, 'data_url')
+      .then(async (snapshot) => {
+        console.log('Upload realizado com sucesso')
+        getDownloadURL(snapshot.ref)
+          .then(async downloadUrl => {
+            console.log(downloadUrl)
+            const docRef = doc(db, `boards/${environmentEntry}/posts/${uid}`)
+            await setDoc(docRef, {
+              text: text,
+              image: downloadUrl,
+              uid: uid
+            })
+          })
+          .then(() => {
+            setPostImg(addPhotoIcon)
+            setText('')
+            setLoadingPosting(false)
+          })
+          .catch((error) => {
+            console.log(error.message)
+            setLoadingPosting(false)
+          })
+      })
+  }
+
+  const handleImgChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      const reader = new FileReader()
+
+      reader.onload = (e: ProgressEvent<FileReader>) => {
+        setPostImg(() => e.target?.result as string)
+        setIsImgUploaded(true)
+      }
+
+      reader.readAsDataURL(e.target.files[0])
+    }
+  }
+
   return (
-    loading
-      ? <LoadingSpinner />
-      : <>
+    <>
       <h4 className='text-center py-2'>{environmentEntry}</h4>
-      <div>Alterar Horário de funcionamento: das {openedAt}hs às {closedAt}hs</div>
-      <form className='form d-flex'>
-        <div>Adicionar Foto</div>
-        <textarea className='form-control' name="text" id="text" placeholder='Deseja escrever algo?' />
-      </form>
-      <div className='d-flex flex-wrap justify-content-center'>
-        <h4 className='text-center py-2'>Publicações</h4>
+      <hr />
+      <p className='text-break text-center'>Alterar Horário de funcionamento:</p>
+      {
+        loading
+          ? <div className='d-flex justify-content-center align-items-center'>
+            <div className="spinner-border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+          : <h5 className='text-break text-center'>
+            <span>
+              das
+              <input
+                className='number-input'
+                type="number"
+                name="openedat"
+                id="openedat"
+                value={openedAt}
+                onChange={e => setOpenedAt(Number(e.target.value))}
+                max={24}
+                min={0}
+              />
+              hs às
+              <input
+                className='number-input'
+                type="number"
+                name="openedat"
+                id="openedat"
+                value={closedAt}
+                max={24}
+                min={0}
+                onChange={e => setClosedAt(Number(e.target.value))}
+              />
+              hs
+              <button onClick={handleOpeningHours} className='btn btn-info hourBtn' disabled={loadingOpeningHours}>
+                {
+                  loadingOpeningHours
+                    ? <div className='d-flex justify-content-center align-items-center'>
+                      <div className="spinner-border" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                      </div>
+                    </div>
+                    : 'Atualizar'
+                }
+              </button>
+            </span>
+          </h5>
+      }
+      <hr />
+      <div className='d-flex mb-3'>
+        <div className="p-3">
+          <div className='mb-3 d-flex justify-content-center'>
+
+            {
+              isImgUploaded
+                ? (
+                  <label htmlFor="photo" className="form-label">
+                    <img
+                      src={postImg}
+                      style={{ width: '100px', height: '100px' }}
+                    />
+                  </label>
+                  )
+                : (
+                  <>
+                    <label htmlFor="photo" className="form-label">
+                      <img
+                        src={postImg}
+                        alt="Foto do usuário"
+                        style={{ width: '100px', height: '100px' }}
+                      />
+                    </label>
+                    <input
+                      className="form-control"
+                      id="photo"
+                      type="file"
+                      name="photo"
+                      accept='image/*'
+                      onChange={handleImgChange}
+                      style={{ display: 'none' }}
+                    />
+                  </>
+                  )
+            }
+            <input
+              className="form-control"
+              id="photo"
+              type="file"
+              name="photo"
+              accept='image/*'
+              onChange={handleImgChange}
+              style={{ display: 'none' }}
+            />
+          </div>
+        </div>
+        <textarea
+          className='form-control'
+          name="text"
+          id="text"
+          placeholder='Deseja escrever algo?'
+          value={text}
+          onChange={e => setText(e.target.value)}
+        />
+      </div>
+      <button onClick={() => handlePost(String(Date.now()))} className='btn btn-info w-100' disabled={loadingPosting}>
         {
-          posts.length
-            ? posts.map(item => <AdminPostCard key={item.uid} imageUrl={item.image} text={item.text} id={item.uid} />)
-            : <div className='w-100 text-center'>Não há notícias por enquanto</div>
+          loadingPosting
+            ? <div className='d-flex justify-content-center align-items-center'>
+              <div className="spinner-border" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </div>
+            </div>
+            : 'Publicar'
+        }
+      </button>
+
+      <hr />
+      <div className='d-flex flex-wrap justify-content-center'>
+        <h4 className='text-center py-2 w-100'>Publicações</h4>
+
+        {
+          loading
+            ? <div className='d-flex justify-content-center align-items-center'>
+              <div className="spinner-border" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </div>
+            </div>
+            : posts.length
+              ? posts.map(item => <AdminPostCard key={item.uid} imageUrl={item.image} text={item.text} id={item.uid} />)
+              : <div className='w-100 text-center'>Não há notícias por enquanto</div>
         }
       </div>
     </>
